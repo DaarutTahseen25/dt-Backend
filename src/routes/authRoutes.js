4280e633e4b19acdc4cecb2eae29b35b44d60258
Abdulazeez Badmus <abdulazeezadekiilekun@gmail.com>
2025-06-28 23:58:13 +0100

    fix: correct the route from /auth/create-sudent to /auth/create-user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { otpVerify, userCreate } from "../controllers/authController";
 
 const router = Router()
-router.post("/auth/create-student", userCreate)
+router.post("/auth/create-user", userCreate)
 
 router.post("/auth/verify-otp", otpVerify)
