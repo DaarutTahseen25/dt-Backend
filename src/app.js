d1e38ff51c351b939f9a8714dcaa2609be83e9f0
Abdulazeez Badmus <abdulazeezadekiilekun@gmail.com>
2025-07-02 06:55:45 +0100

    fix: build error

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import studentRoutes from './routes/studentRoutes.js';
 import homeRoutes from './routes/homeRoutes.js';
 import healthRoutes from './routes/healthRoutes.js';
 import errorHandler from './middleware/errorMiddleware.js';
@@ -9,7 +8,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/', homeRoutes);
-app.use('/api/students', studentRoutes);
 app.use('/api/health', healthRoutes);
 
 //middlewares
